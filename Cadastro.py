10c3942edfcde0dc45267bc211dca656e61a3b43
llpalm <145064132+llpalm@users.noreply.github.com>
2023-09-15 13:22:09 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import openpyxl
 st.title("Informações Gerais")
 
 #suporte = pd.read_excel(r'C:\Users\lpalmeira\Videos\Streamlit\data\Analise_Fim.xlsx')
