0e0452de98b876913b0f70d23aae8ee36cb67647
llpalm <145064132+llpalm@users.noreply.github.com>
2023-09-15 17:34:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ st.title('Análise de Viabilidade de Projeto - Capex')
 #st.set_page_config (page_title="Modelagem Financeira Capex" )
 st.title('Cadastro')
 
-@st.cache_data
-def carregar_dados():
-        suporte = pd.read_excel("Analise_Fim.xlsx")
-        return suporte
+# @st.cache_data
+# def carregar_dados():
+#         suporte = pd.read_excel("Analise_Fim.xlsx")
+#         return suporte
 
 st.sidebar.success('Menu de Navegação')
 
