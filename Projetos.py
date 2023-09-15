0d2da4356ee26dccadc307ae6b5ab19a1b00a4eb
llpalm <145064132+llpalm@users.noreply.github.com>
2023-09-15 13:22:33 -0300

    Update Projetos.py

--- Projetos.py
+++ Projetos.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import openpyxl
 st.title("Projetos")
 
 resultado = pd.read_excel(r'C:\Users\lpalmeira\Videos\Streamlit\data\Analise_Teste_Saida.xlsx')
