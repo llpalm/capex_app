15d681e02b690ab54ed946353d89fd492efebc77
llpalm <145064132+llpalm@users.noreply.github.com>
2023-09-15 18:42:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ arquivo = st.file_uploader(
     'Suba seu arquivo aqui!'
 )
 
-st.write(arquivo)
+st.code(arquivo.read().decode())
 # @st.cache_data
 # def carregar_dados():
 #         suporte = pd.read_excel("Analise_Fim.xlsx")
